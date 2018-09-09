9bd492bcf31bc6cd4b39f76935f4b752354cd192
Akkadius <akkadius1@gmail.com>
2018-09-09 03:50:38 -0500

    Adjust source-database.sh

--- scripts/source-database.sh
+++ scripts/source-database.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-mysql -h mariadb -u root -proot -e "DROP DATABASE peq; CREATE DATABASE peq; GRANT ALL PRIVILEGES ON  peq.* to 'eqemu'@'%' WITH GRANT OPTION;"
+mysql -h mariadb -u root -proot -e "DROP DATABASE IF EXISTS peq; CREATE DATABASE peq; GRANT ALL PRIVILEGES ON  peq.* to 'eqemu'@'%' WITH GRANT OPTION;"
